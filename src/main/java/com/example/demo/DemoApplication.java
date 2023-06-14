bef8adf2e28bfa7a90fc4635dd4cfc95c690db02
Alejandro <tozcuento.2829@gmail.com>
2023-06-14 14:43:04 -0600

    third commit with errors using pmd.bat

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.printl("Test de salida"):
+		ystem.out.printl("Test de salida"):
 	}   
 }
