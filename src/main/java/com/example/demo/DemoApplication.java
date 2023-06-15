e60f0cb088f84c087d522e8a07ed38e24bb02af5
Alejandro <tozcuento.2829@gmail.com>
2023-06-15 12:26:34 -0600

    test not using ; in sout

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.printl("test sin ;")
+		System.out.printl("test sin ;")--
 	}   
 }
