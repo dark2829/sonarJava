b0a6599039f425f0ef6a3073a07a53ff30ea917c
Alejandro <tozcuento.2829@gmail.com>
2023-06-26 14:47:18 -0600

    test whit min proyect 3

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -9,4 +9,10 @@ public class User {
     private String username;
     private String email;
 
+    public User(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,40 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.model.User;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
 
-	private List<User> users = new ArrayList<>();
-
-	public List<User> getAllUsers() {
-		return users;
-	}
-
-	public User getUserById(Long id) {
-		// Implementación para obtener un usuario por su ID
-            return null;
-		// Implementación para obtener un usuario por su ID
-	}
-
-	public User createUser(User user) {
-		// Implementación para crear un nuevo usuario
-            return null;
-		// Implementación para crear un nuevo usuario
-	}
-
-	public User updateUser(Long id, User user) {
-		// Implementación para actualizar un usuario existente
-            return null;
-		// Implementación para actualizar un usuario existente
-	}
-
-	public void deleteUser(Long id) {
-		// Implementación para eliminar un usuario por su ID
-	}
+    public void addUser(String username, String email) {
+        // Lógica para agregar un usuario
+    }
 
 }
