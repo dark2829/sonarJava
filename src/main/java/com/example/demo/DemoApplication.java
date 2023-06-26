a76ff55abbb39c1ff0ef4b71d827ca897e00f317
Alejandro <tozcuento.2829@gmail.com>
2023-06-26 11:37:29 -0600

    return code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		String cls = "texto"; 
 		SpringApplication.run(DemoApplication.class, args);
-		log.info("A");
+		log.info("Ac");
 	}   
 	
 }
