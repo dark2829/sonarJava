405dd898619ccc814f4b1f88728661201bbf5d20
Alejandro <tozcuento.2829@gmail.com>
2023-06-26 11:03:10 -0600

    acept code, all rules accept

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,29 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        String cls = "texto"; 
         SpringApplication.run(DemoApplication.class, args);
-        log.info("A");
+        log.info("Application started");
     }   
-    
-    public void unnecessaryMethod() {
-        int x = 10;
-        int y = 20;
-        int sum = x + y;
-        System.out.println("Sum: " + sum);
-    }
-    
-    public void emptyMethod() {
-        
-    }
-    
-    public void invalidMethodName() {
-        int _abc = 10;
-        System.out.println(_abc);
-    }
-    
-    public void unnecessaryComment() {
-        // This is an unnecessary comment
-        System.out.println("Hello");
-    }
 }
