c14d5397c876aef3ed1c4ab140483ed6fa34255f
Alejandro <tozcuento.2829@gmail.com>
2023-06-26 11:01:08 -0600

    error code, all rules failed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,10 +8,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		String cls = "texto"; 
-		SpringApplication.run(DemoApplication.class, args);
-		log.info("A");
-	}   
-	
+    public static void main(String[] args) {
+        String cls = "texto"; 
+        SpringApplication.run(DemoApplication.class, args);
+        log.info("A");
+    }   
+    
+    public void unnecessaryMethod() {
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("Sum: " + sum);
+    }
+    
+    public void emptyMethod() {
+        
+    }
+    
+    public void invalidMethodName() {
+        int _abc = 10;
+        System.out.println(_abc);
+    }
+    
+    public void unnecessaryComment() {
+        // This is an unnecessary comment
+        System.out.println("Hello");
+    }
 }
