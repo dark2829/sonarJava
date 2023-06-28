c91edc29bdce73a96be9d272493c12c2bb112316
Alejandro <tozcuento.2829@gmail.com>
2023-06-28 11:18:52 -0600

    code ok

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,15 +11,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         log.info("Application started successfully");
-        List<String> nombres = new ArrayList<>();
-        nombres.add("Juan");
-        nombres.add("María");
-        nombres.add("Pedro");
-
-        // Ejemplo con bucle 'for' que causa un error
-        for (String nombre : nombres) {
-            System.out.println(nombre);
-        }
     }
 
 }
