39dc7920ede497eddd1c45a67cb57613cd051990
Alejandro <tozcuento.2829@gmail.com>
2023-06-28 11:16:25 -0600

    usign for, not for each

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,15 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         log.info("Application started successfully");
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Juan");
+        nombres.add("María");
+        nombres.add("Pedro");
+
+        // Ejemplo con bucle 'for' que causa un error
+        for (int i = 0; i <= nombres.size(); i++) {
+            System.out.println(nombres.get(i));
+        }
     }
 
 }
