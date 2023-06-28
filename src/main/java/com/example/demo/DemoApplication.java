e5ea7ce2c6baca77e1b9c557f34a3ee48da7ac1f
Alejandro <tozcuento.2829@gmail.com>
2023-06-28 11:55:55 -0600

    using imports, short var

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import java.util.List;
 @SpringBootApplication
 @Slf4j
 public class DemoApplication {
-    String a = "a"
+    a
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
