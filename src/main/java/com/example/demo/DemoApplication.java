a7b1264bc41250edee6f327ea64e7c4ef5d3cd66
Alejandro <tozcuento.2829@gmail.com>
2023-06-29 12:46:03 -0600

    test rule not imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,6 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 @Slf4j
