79b6bb61a4cf905074fae88c56c6a7744ddc2fd4
Alejandro <tozcuento.2829@gmail.com>
2023-06-29 10:05:45 -0600

    imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        log.info("Application started successfully")
     }
 
 }
