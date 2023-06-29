928e10fc26d9d908ccca53493378b83557a751e2
Alejandro <tozcuento.2829@gmail.com>
2023-06-29 13:06:29 -0600

    test rule foreach

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @Slf4j
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Juan");
+        nombres.add("María");
+        nombres.add("Pedro");
+        nombres.add("Ana");
+
+        // Usando un bucle for
+        for (int i = 0; i < nombres.size(); i++) {
+            System.out.println(nombres.get(i));
+        }
     }
 
 }
